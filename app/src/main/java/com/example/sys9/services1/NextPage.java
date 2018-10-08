c2219480449e3e61cf9e0d2163c8a439ad8abf66
Nirosha <Niroshareddy85@gmail.com>
2018-10-08 12:04:12 -0700

    task

---
+++ app/src/main/java/com/example/sys9/services1/NextPage.java
@@ -0,0 +1,14 @@
+package com.example.sys9.services1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+class NextPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
+
